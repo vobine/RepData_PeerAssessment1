cd31c42ee727df6fe5e2626580120c8e005b456a
Hal Peterson <vobine@users.noreply.github.com>
2015-01-17 09:16:27 -0600

    This works: impute mean by interval, then grand mean

--- fixNA.R
+++ fixNA.R
@@ -1,3 +1,12 @@
-fixNA <- function (stepData) {
-  print (sum (is.na (stepData$steps)))
+fixNA <- function (stepData, perInterval) {
+  which <- is.na (stepData$steps)
+  print (sum (which));
+  
+  fixie <- stepData;
+  fixie$steps[which] <-
+    perInterval$mst[perInterval$interval == fixie$interval[which]];
+  fixie$steps[is.na (fixie$steps)] <- 
+    mean (fixie$steps, na.rm=TRUE);
+  
+  return (fixie);
 }
